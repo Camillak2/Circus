d18029cc914ddc0c0919461b94ee8a0c5e4ec762
Camillak2 <ksagdieva2@gmail.com>
2024-02-22 14:36:45 +0300

    Добавьте файлы проекта.

---
+++ Circus/DB/DBConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace Circus.DB
+{
+    internal class DBConnection
+    {
+        public static Circus_SagdievaEntities circusDB = new Circus_SagdievaEntities();
+
+        public static Worker loginedWorker;
+
+        public static Worker selectedForEditWorker;
+    }
+}
